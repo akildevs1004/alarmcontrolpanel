d75eda370609c416ddee69ab06d994e0e017430f
Venu J <149071174+akildevs1004@users.noreply.github.com>
2024-07-18 19:27:38 +0400

    notificaton

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -243,73 +243,6 @@
       </span>
 
       <v-spacer></v-spacer>
-
-      <v-menu
-        nudge-bottom="50"
-        transition="scale-transition"
-        origin="center center"
-        bottom
-        left
-        min-width="200"
-        nudge-left="20"
-      >
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn icon color="red" v-bind="attrs" v-on="on">
-            <v-avatar size="35" style="border: 1px solid #6946dd">
-              <v-img class="company_logo" :src="getLogo"></v-img>
-            </v-avatar>
-          </v-btn>
-        </template>
-
-        <v-list light nav dense>
-          <v-list-item-group color="primary">
-            <v-list-item
-              v-if="$auth && $auth.user.user_type == 'company'"
-              @click="goToCompany()"
-            >
-              <v-list-item-icon>
-                <v-icon>mdi-account-multiple-outline</v-icon>
-              </v-list-item-icon>
-              <v-list-item-content>
-                <v-list-item-title class="black--text"
-                  >Profile</v-list-item-title
-                >
-              </v-list-item-content>
-            </v-list-item>
-
-            <v-list-item @click="logout">
-              <v-list-item-icon>
-                <v-icon>mdi-logout</v-icon>
-              </v-list-item-icon>
-              <v-list-item-content>
-                <v-list-item-title class="black--text"
-                  >Logout</v-list-item-title
-                >
-              </v-list-item-content>
-            </v-list-item>
-          </v-list-item-group>
-        </v-list>
-      </v-menu>
-
-      <!-- <v-btn icon plan @click="toggleTheme">
-        <v-icon
-          >mdi-{{
-            $vuetify.theme.dark ? "white-balance-sunny" : "moon-waning-crescent"
-          }}</v-icon
-        >
-      </v-btn>  -->
-
-      <!-- <v-btn
-        v-if="getLoginType == 'company' || getLoginType == 'branch'"
-        icon
-        plan
-        @click="goToSettings()"
-        class="mr-3"
-        ><v-icon class="violet--text" style="text-align: center"
-          >mdi-cog</v-icon
-        ></v-btn
-      > -->
-
       <v-menu
         style="z-index: 9999 !important"
         bottom
@@ -378,6 +311,71 @@
           </v-list-item>
         </v-list>
       </v-menu>
+      <v-menu
+        nudge-bottom="50"
+        transition="scale-transition"
+        origin="center center"
+        bottom
+        left
+        min-width="200"
+        nudge-left="20"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn icon color="red" v-bind="attrs" v-on="on">
+            <v-avatar size="35" style="border: 1px solid #6946dd">
+              <v-img class="company_logo" :src="getLogo"></v-img>
+            </v-avatar>
+          </v-btn>
+        </template>
+
+        <v-list light nav dense>
+          <v-list-item-group color="primary">
+            <v-list-item
+              v-if="$auth && $auth.user.user_type == 'company'"
+              @click="goToCompany()"
+            >
+              <v-list-item-icon>
+                <v-icon>mdi-account-multiple-outline</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title class="black--text"
+                  >Profile</v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+
+            <v-list-item @click="logout">
+              <v-list-item-icon>
+                <v-icon>mdi-logout</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title class="black--text"
+                  >Logout</v-list-item-title
+                >
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-menu>
+
+      <!-- <v-btn icon plan @click="toggleTheme">
+        <v-icon
+          >mdi-{{
+            $vuetify.theme.dark ? "white-balance-sunny" : "moon-waning-crescent"
+          }}</v-icon
+        >
+      </v-btn>  -->
+
+      <!-- <v-btn
+        v-if="getLoginType == 'company' || getLoginType == 'branch'"
+        icon
+        plan
+        @click="goToSettings()"
+        class="mr-3"
+        ><v-icon class="violet--text" style="text-align: center"
+          >mdi-cog</v-icon
+        ></v-btn
+      > -->
 
       <!-- <v-menu
         bottom
