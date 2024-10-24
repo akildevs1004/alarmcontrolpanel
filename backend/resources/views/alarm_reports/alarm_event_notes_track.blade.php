87aa76284617567a79bd73cb8b3be4c417b69a51
Venu J <149071174+akildevs1004@users.noreply.github.com>
2024-10-24 18:50:27 +0400

    Notes

--- backend/resources/views/alarm_reports/alarm_event_notes_track.blade.php
+++ backend/resources/views/alarm_reports/alarm_event_notes_track.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{$alarm->id}} Track Notes</title>
+    <title>Alarm Event #{{$alarm->id}} - Track Notes</title>
     <style>
         body {
             font-family: Arial, sans-serif;
