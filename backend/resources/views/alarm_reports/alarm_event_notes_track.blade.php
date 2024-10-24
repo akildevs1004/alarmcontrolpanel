ce0d11c87ad40ed8c467bcce53fecde4ad78c679
Venu J <149071174+akildevs1004@users.noreply.github.com>
2024-10-24 19:00:37 +0400

    Heihgt

--- backend/resources/views/alarm_reports/alarm_event_notes_track.blade.php
+++ backend/resources/views/alarm_reports/alarm_event_notes_track.blade.php
@@ -258,7 +258,7 @@
             <hr style="color:#ddd;margin-top:0px " />
 
             @if (count($alarm->notes)==0)
-            <div style="width:100%;height:200px;margin:auto;font-size:12px;text-align:center">
+            <div style="width:100%;height:50px;margin:auto;font-size:12px;text-align:center">
                 <div style="margin:auto">
                     Operator Notes are not available
                 </div>
