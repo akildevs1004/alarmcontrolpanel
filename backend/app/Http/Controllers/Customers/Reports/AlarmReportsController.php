a57a39c2d111475741fb0ba00977d2e814b8aadd
Venu J <149071174+akildevs1004@users.noreply.github.com>
2024-10-24 15:59:57 +0400

    1529

--- backend/app/Http/Controllers/Customers/Reports/AlarmReportsController.php
+++ backend/app/Http/Controllers/Customers/Reports/AlarmReportsController.php
@@ -93,7 +93,7 @@ class AlarmReportsController extends Controller
     {
         $alarmId = 1529;
 
-        return $alarm =   AlarmEvents::with([
+        $alarm =   AlarmEvents::with([
             "device.customer.primary_contact",
             "device.customer.secondary_contact",
             "device.company.user",
