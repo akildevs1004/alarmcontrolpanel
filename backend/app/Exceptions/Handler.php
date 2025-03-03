90cd8b27ea6056801ac3c2eecf7f266eec7020f5
Venu J <149071174+akildevs1004@users.noreply.github.com>
2025-03-03 15:00:21 +0400

    test

--- backend/app/Exceptions/Handler.php
+++ backend/app/Exceptions/Handler.php
@@ -1,11 +1,10 @@
 <?php
 
+
 namespace App\Exceptions;
 
-use Illuminate\Support\Facades\Mail;
-use App\Mail\ErrorOccurred;
-use Exception;
-use PhpOffice\PhpSpreadsheet\Calculation\ExceptionHandler;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Throwable;
 
 class Handler extends ExceptionHandler
 {
@@ -43,32 +42,34 @@ class Handler extends ExceptionHandler
      *
      * @return void
      */
-    // public function register()
+    public function register()
+    {
+        $this->reportable(function (Throwable $e) {
+            //
+        });
+    }
+
+
+    // public function report(Exception $exception)
     // {
-    //     $this->reportable(function (Throwable $e) {
-    //         $this->sendErrorEmail($e);
-    //     });
+
+    //     // parent::report($exception);
+
+    //     $this->sendErrorEmail($exception);
     // }
-    public function report(Exception $exception)
-    {
-
-        // parent::report($exception);
-
-        $this->sendErrorEmail($exception);
-    }
 
 
-    protected function sendErrorEmail(\Exception $exception)
-    {
-        // Prepare the error details
-        $errorDetails = [
-            'exception_message' => $exception->getMessage(),
-            'exception_file' => $exception->getFile(),
-            'exception_line' => $exception->getLine(),
-            'stack_trace' => $exception->getTraceAsString(),
-        ];
+    // protected function sendErrorEmail(\Exception $exception)
+    // {
+    //     // Prepare the error details
+    //     $errorDetails = [
+    //         'exception_message' => $exception->getMessage(),
+    //         'exception_file' => $exception->getFile(),
+    //         'exception_line' => $exception->getLine(),
+    //         'stack_trace' => $exception->getTraceAsString(),
+    //     ];
 
-        Mail::to('xtremegurad@gmail.com')->send(new ErrorOccurred($errorDetails));
-        Mail::to('venuakil2@gmail.com')->send(new ErrorOccurred($errorDetails));
-    }
+    //     Mail::to('xtremegurad@gmail.com')->send(new ErrorOccurred($errorDetails));
+    //     Mail::to('venuakil2@gmail.com')->send(new ErrorOccurred($errorDetails));
+    // }
 }
