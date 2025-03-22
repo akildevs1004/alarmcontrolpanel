0275a5bbb3b734cba53c5baa80df0dbbd16820bc
Venu J <149071174+akildevs1004@users.noreply.github.com>
2025-03-22 14:55:15 +0400

    Exception Logging Fixed

--- backend/app/Exceptions/Handler.php
+++ backend/app/Exceptions/Handler.php
@@ -1,86 +1,62 @@
 <?php
 
-
 namespace App\Exceptions;
 
 use App\Mail\ErrorOccurred;
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * A list of exception types with their corresponding custom log levels.
-     *
-     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
-     */
-    protected $levels = [
-        //
-    ];
+    protected $levels = [];
 
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array<int, class-string<\Throwable>>
-     */
-    protected $dontReport = [
-        //
-    ];
+    protected $dontReport = [];
 
-    /**
-     * A list of the inputs that are never flashed to the session on validation exceptions.
-     *
-     * @var array<int, string>
-     */
     protected $dontFlash = [
         'current_password',
         'password',
         'password_confirmation',
     ];
 
-    /**
-     * Register the exception handling callbacks for the application.
-     *
-     * @return void
-     */
+    public function report(Throwable $exception)
+    {
+        Log::error($exception->getMessage(), ['exception' => $exception]);
+
+        // Prevent duplicate email sending
+        if (!$this->shouldntReport($exception)) {
+            $this->sendErrorEmail($exception);
+        }
+
+        parent::report($exception);
+    }
+
     public function register()
     {
-        // $this->reportable(function (Throwable $e) {
-
-        // });
-
-        // $this->reportable(function (Exception $exception) {
-        //     $this->sendErrorEmail($exception);
-        // });
-
-        $this->reportable(function (Throwable $exception) {
-            $this->sendErrorEmail($exception);
+        $this->reportable(function (Throwable $e) {
+            Log::error('An error occurred: ' . $e->getMessage());
         });
     }
 
-
-    // public function report(Exception $exception)
-    // {
-
-    //     // parent::report($exception);
-
-    //     $this->sendErrorEmail($exception);
-    // }
-
-
-    protected function sendErrorEmail(\Exception $exception)
+    protected function sendErrorEmail(Throwable $exception)
     {
-        // Prepare the error details
-        $errorDetails = [
-            'exception_message' => $exception->getMessage(),
-            'exception_file' => $exception->getFile(),
-            'exception_line' => $exception->getLine(),
-            'stack_trace' => $exception->getTraceAsString(),
-        ];
+        try {
+            $errorDetails = [
+                'exception_message' => $exception->getMessage(),
+                'exception_file' => $exception->getFile(),
+                'exception_line' => $exception->getLine(),
+                'stack_trace' => $exception->getTraceAsString(),
+            ];
 
-        Mail::to('xtremegurad@gmail.com')->send(new ErrorOccurred($errorDetails));
-        Mail::to('venuakil2@gmail.com')->send(new ErrorOccurred($errorDetails));
+            $recipients = ['xtremegurad@gmail.com', 'venuakil2@gmail.com'];
+
+            foreach ($recipients as $email) {
+                Mail::to($email)->send(new ErrorOccurred($errorDetails));
+            }
+        } catch (Throwable $mailException) {
+            Log::error("Error sending error email: " . $mailException->getMessage());
+        }
     }
 }
